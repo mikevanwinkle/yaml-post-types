bad119dec6b1c602f73cdb36c15d1724fe20a68d
Mike Van Winkle <mike@mikevanwinkle.com>
2016-01-04 15:20:40 -0800

    Cleanup spacing

--- yaml-post-types.php
+++ yaml-post-types.php
@@ -20,7 +20,7 @@ Class Yamlfy {
     require_once dirname(__FILE__) .'/vendor/autoload.php';
     if (!class_exists('cmb2_bootstrap_202')) {
       require_once dirname(__FILE__) .'/vendor/cmb2/init.php';
-			require_once dirname(__FILE__) .'/vendor/cmb2-attached-posts/cmb2-attached-posts-field.php';
+      require_once dirname(__FILE__) .'/vendor/cmb2-attached-posts/cmb2-attached-posts-field.php';
     }
     add_action('init',array( $this, 'init' ));
   }
@@ -32,36 +32,36 @@ Class Yamlfy {
     return self::$instance;
   }
 
-	public function loadConfigs() {
+  public function loadConfigs() {
       $parser = new \Symfony\Component\Yaml\Parser(); 
       $finder = new \Symfony\Component\Finder\Finder();
-			if( !$configs = get_transient('yaml-post-types-configs') ) {
-	      $dirs = apply_filters('yamlfy-config-dirs', array(dirname(__FILE__)));
-  	    $finder->name('*.yaml')->files()->in($dirs)->notPath('tests');
-    	  $configs = array();
-      	foreach ( $finder as $file ) {
-        	  $configs[] = $parser->parse( $file->getContents() );
-      	}
-				set_transient('yaml-post-types-configs', $configs);
-			}
-			return $configs;
-	}
+      if( !$configs = get_transient('yaml-post-types-configs') ) {
+        $dirs = apply_filters('yamlfy-config-dirs', array(dirname(__FILE__)));
+        $finder->name('*.yaml')->files()->in($dirs)->notPath('tests');
+        $configs = array();
+        foreach ( $finder as $file ) {
+            $configs[] = $parser->parse( $file->getContents() );
+        }
+        set_transient('yaml-post-types-configs', $configs);
+      }
+      return $configs;
+  }
 
   public function init() {
     try {
-			$configs = $this->loadConfigs();
-			foreach($configs as $config) {
-  			$post_types = $taxonomies = $meta_boxes = $index = false;
-  			if (array_key_exists('post_types', $config)) {
-  				$this->register_post_types($config['post_types']);	
-	  		}
+      $configs = $this->loadConfigs();
+      foreach($configs as $config) {
+        $post_types = $taxonomies = $meta_boxes = $index = false;
+        if (array_key_exists('post_types', $config)) {
+          $this->register_post_types($config['post_types']);  
+        }
 
-		  	if (array_key_exists('taxonomies',$config)) {
-			  	$this->register_taxonomies($config['taxonomies']);
-			  }
-	  		if (array_key_exists('meta_boxes',$config)) {
-		  		$this->load_meta_boxes($config['meta_boxes']);
-		  	}
+        if (array_key_exists('taxonomies',$config)) {
+          $this->register_taxonomies($config['taxonomies']);
+        }
+        if (array_key_exists('meta_boxes',$config)) {
+          $this->load_meta_boxes($config['meta_boxes']);
+        }
       }
     } catch (\Symfony\Component\Yaml\Exception\ParseException $e) {
         $this->error("Could not parse {$file->getFilename()}:{$e->getMessage()}");
@@ -70,26 +70,26 @@ Class Yamlfy {
     }
   }
 
-	public function register_post_types($post_types) {
-		foreach($post_types as $post_type => $args) {
-			register_post_type($post_type,$args);
-		}
-	}
+  public function register_post_types($post_types) {
+    foreach($post_types as $post_type => $args) {
+      register_post_type($post_type,$args);
+    }
+  }
 
-	public function register_taxonomies($taxonomies) {
-		foreach ($taxonomies as $taxonomy => $info) {
-			register_taxonomy($taxonomy, $info['object_type'], $info['args']);
-		}
-	}
+  public function register_taxonomies($taxonomies) {
+    foreach ($taxonomies as $taxonomy => $info) {
+      register_taxonomy($taxonomy, $info['object_type'], $info['args']);
+    }
+  }
 
   public function load_meta_boxes($meta_boxes) {
     $this->mbs = $meta_boxes;
-		add_filter("cmb2_meta_boxes", array($this, 'merge_mbs'));
+    add_filter("cmb2_meta_boxes", array($this, 'merge_mbs'));
   }
 
-	public function merge_mbs($mbs) {
-		return array_merge($mbs, $this->mbs);
-	}
+  public function merge_mbs($mbs) {
+    return array_merge($mbs, $this->mbs);
+  }
 
   public function error($message) {
     $this->error = $message;
